4d5254c7efd6eadedfa26c81107d53c773ee1eeb
qxz-coder <1316075793@qq.com>
2021-10-05 18:46:15 +0800

    finish search_str function

--- September/SearchString_v1.py
+++ September/SearchString_v1.py
@@ -159,35 +159,36 @@ def output_arsc_strings(a):
     fout_s.close()
     return arsc_str_id,arsc_str
                     
-# def  fliter_file(file_path):
-#     # 替换文件中的字符串
-#     replaceStr ="R$string" 
-#     with open(file_path,'r',encoding="utf-8") as r:
-#         lines=r.readlines()
-#     with open(file_path,'w',encoding="utf-8") as w:
-#         for l in lines:
-#             if replaceStr not in l:
-#              w.write(l) 
+def  fliter_file(file_path):
+    # 替换文件中的字符串
+    replaceStr ="R$string" 
+    with open(file_path,'r',encoding="utf-8") as r:
+        lines=r.readlines()
+    with open(file_path,'w',encoding="utf-8") as w:
+        for l in lines:
+            if replaceStr not in l:
+             w.write(l) 
     
-#删除指定文件名中含指定字符串的文件  用于删除R$string.smali
-def deletefile(start, name):
-    for relpath, _, files in os.walk(start):
-        if name in files:
-            full_path = os.path.join(start, relpath, name)
-            print(os.path.normpath(os.path.abspath(full_path)))
-            os.remove(os.path.normpath(os.path.abspath(full_path))) 
+
+def search_str(dx,str,fout):
+    #搜索instructions中使用的整数
+    for method in dx.get_methods():    
+        if method.is_external():
+            continue
+        m = method.get_method()
+        for ins in m.get_instructions():
+            # print(ins.get_op_value(), ins.get_name(), ins.get_output())
+            # print( ins.get_hex(),ins.get_raw())                          class_name: get_class_name()    method_name:get_name()
+            output=ins.get_output()
+            if str in output:
+                fout.write(ins.get_name()+output+m.get_class_name()+m.get_name()+"\n")
 
 
 # 要分析的apk路径
 # sp = 'D:\\workspace\\Android-immunity\\androguard_attempt\\19300240012.apk'
 # sp = 'D:\\文件\\学习\\课程\\曦源项目\\mwallet\\com.telkom.mwallet_101_apps.evozi.com.apk'
-file_path = "C:\\Users\\86157\\Desktop\\Andguard\\news.apk"
+file_path = "C:\\Users\\86157\\Desktop\\Andguard\\com.caremark.caremark.apk"
 store_file_path="C:\\Users\\86157\\Desktop\\Andguard\\"
-smali_file_path="C:\\Users\\86157\\Desktop\\Andguard\\smali_out"
-work_file_path=store_file_path
-arsc_file_path = store_file_path + "arsc_result.txt"
-
-delete_string="R$string.smali"
 
 
 if __name__ == '__main__':
@@ -210,55 +211,13 @@ if __name__ == '__main__':
     output_calling_method_neg(strs)
     print("output_negstr in program finish")
 
-    #搜索str_arsc中的内容,每一行都是一个十进制id
-
-    #基本思路：匹配以smali为后缀的文件，在其中搜索对nameid的使用
-        
-    # 使用chdir进入apktool工作目录
-    os.chdir(work_file_path)
-
-
-    #配置apktool 环境， 参考:https://blog.csdn.net/ruancoder/article/details/51924179
-    # 使用apktool 对apk文件进行反编译得到smali文件 输出文件名：smali_out
-    smali_file = Path(smali_file_path)
-    if not smali_file.exists():
-        print("no smali_out!!")  
-        os.system("apktool d -r -o smali_out "+file_path)
-    else:
-        print("smali file exists!!!")
-
-    # 删除smali中所有R$sring.smali
-    # 慎用此函数，文件一经删除则无法恢复
-    # 检索较慢，或者第一次运行时可以适当调用该函数
-    # deletefile(smali_file_path,delete_string)
-
-
-    # 使用echo 重置或创建result.txt文件
-    os.system("echo asrc string search result: > arsc_result.txt")
-
-
-    os.chdir(smali_file_path)
+    fout_arsc = open(store_file_path+"arsc_result.txt", "w+", encoding='utf-8')
+    print("begin to search arsc")
     for i in range(0,len(s)):
-        s_dec=int(s[i],10)
-        s_hex=hex(s_dec)
-        serach_str=str(s_hex)
-
-        # 使用echo向文件中输入分割线与提示内容
-        os.system("echo ------separationline---------- >> "+arsc_file_path)
-        os.system("echo search stringID: "+serach_str+" >> "+arsc_file_path)
-        os.system("echo search string content: " + string[i] + " >> "+arsc_file_path)
-        os.system("echo search result: >> "+arsc_file_path)
-
-        #使用findstr命令递归搜索当前目录及其子目录下的所有smali文件，并追加到arsc_result.txt文件中
-        #findstr 相关参数与介绍 参考：https://www.netingcn.com/window-findstr-command.html
-        os.system("findstr /MSI \""+serach_str+"\" *.smali >> "+arsc_file_path)
-        # os.system("findstr /MSI \""+serach_str+"\" *.smali >> arsc_result.txt")
-        os.system("echo ------separationline---------- >> "+arsc_file_path)
-
-        # 重新处理输出文件去除“R$string"部分
-        # arsc_file_path = store_file_path + "arsc_result.txt"
-    # fliter_file(arsc_file_path)
-
-
-
+        # print(s[i])
+        fout_arsc.write("search ID: "+s[i]+"\n")
+        fout_arsc.write("search string content: " + string[i]+"\n")
+        fout_arsc.write("search result: \n")
+        search_str(dx,str(s[i]),fout_arsc)
+        fout_arsc.write("------separationline----------\n")
     print("语义分析完成")
