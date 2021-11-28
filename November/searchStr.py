2fb0b6c60161c42b194bbf9359a3dfd15b49ccd1
qxz-coder <1316075793@qq.com>
2021-11-28 13:39:55 +0800

    add find logic method

--- November/searchStr.py
+++ November/searchStr.py
@@ -14,6 +14,8 @@ global blackList,whiteList
 blackList = ['encrypt','algorithm','decrypt', 'null', 'index out of range','Dead object', 'null', 'msg', 'info', 'invoke', 'call', 'open', 'close','resolve','bind','Google','Expected','registration','account','Account','View','view','message','cipher','Cipher','retrieving','color','Layout','layout','Shared Preferences','upload','object','URL','password','zero','empty','center','response','Failed','failed']
 whiteList = ['root','device','debug','exit','fatal','malicious', 'sorry','please','safe','/su','security']
 
+# smali 代码中条件语句
+Conditionlist=['if-ne','if-eq','if-lt','if-le','if-gt','if-ge','if-eqz','if-nez','if-ltz','if-lez','if-gtz','if-gez']
 
 
 # 对apk进行处理
@@ -84,7 +86,7 @@ def is_all_chinese(strs):
 def output_calling_method_neg(strs):
     cnt = 1
     fout_n = open(store_file_path+"output_neg.txt", "w+",encoding='utf-8')
-
+    neg_string=[]
     for s in strs:
         # 使用TextBlob处理
         next = 0 # 是否已经在白名单中被写过
@@ -106,6 +108,7 @@ def output_calling_method_neg(strs):
             for item in whiteList:
                 if item in text:
                     fout_n.write("[%d]%s" % (cnt, text))
+                    neg_string.append(text)
                     cnt += 1
                     fout_n.write("\nwhiteList:{} \tUsed in {} -- {}".format(item,meth.class_name,meth.name))
                     fout_n.write('\n\n')
@@ -126,6 +129,7 @@ def output_calling_method_neg(strs):
             if score < 0:
                 try:
                     fout_n.write("[%d]%s || %f" % (cnt, text, score))
+                    neg_string.append(text)
                     cnt += 1
                     fout_n.write("\n\tUsed in {} -- {}".format(meth.class_name,meth.name))
                     fout_n.write('\n\n')
@@ -137,6 +141,7 @@ def output_calling_method_neg(strs):
             for item in whiteList:
                 if item in text:
                     fout_n.write("[%d]%s" % (cnt, text))
+                    neg_string.append(text)
                     cnt += 1
                     fout_n.write("\nwhiteList:\tUsed in {} -- {}".format(meth.class_name,meth.name))
                     fout_n.write('\n\n')
@@ -160,6 +165,7 @@ def output_calling_method_neg(strs):
                         continue
                     try:
                         fout_n.write("[%d]%s || %f" % (cnt, text, score2))
+                        neg_string.append(text)
                         cnt += 1
                         fout_n.write("\n\tUsed in {} -- {}".format(meth.class_name,meth.name))
                         fout_n.write('\n\n')
@@ -168,6 +174,7 @@ def output_calling_method_neg(strs):
                 
         
     fout_n.close()
+    return neg_string
 
 
 def output_arsc_strings(a):
@@ -226,19 +233,28 @@ def  fliter_file(file_path):
              w.write(l) 
     
 
-def search_str(dx,str,fout):
+def search_str(dx,str,fout,fout2,string):
     #搜索instructions中使用的整数
     for method in dx.get_methods():    
         if method.is_external():
             continue
         m = method.get_method()
+        find_logical=0
         for ins in m.get_instructions():
-            print(ins.get_op_value(), ins.get_name(), ins.get_output())
-            os.system("pause")
+            # print(ins.get_op_value(), ins.get_name(), ins.get_output())
+            # os.system("pause")
             # print( ins.get_hex(),ins.get_raw())                          class_name: get_class_name()    method_name:get_name()
-            output=ins.get_output()
+            output=ins.get_name()+ins.get_output()
             if str in output:
                 fout.write(ins.get_name()+output+m.get_class_name()+m.get_name()+"\n")
+                if find_logical==0:
+                    find_logical=find_logical+1
+                    fout2.write("search ID: "+str+"\n")
+                    fout2.write("search string content: " + string+"\n")
+                    fout2.write(ins.get_name()+output+m.get_class_name()+m.get_name()+"\n")
+                    fout2.write("search logic result: \n")
+                    get_logical_method(method,dx,fout2)
+                    fout2.write("------separationline----------\n\n")
 
 
 # 要分析的apk路径
@@ -281,7 +297,7 @@ def getarg(argv):
 def read_exception_list(path):
     global exception_list
     result = []
-    with open(exception_list_path,'r') as f:
+    with open(path,'r') as f:
         for line in f:
             result.append(line.strip('\n').split(','))
     merge = sum(result,[])
@@ -294,11 +310,85 @@ def read_exception_list(path):
 
 
 
+
+#  基于get_xref_from/to api寻找 字符串、方法可能的 调用条件
+#  第一个参数为string或者method 类型，第二个参数是分析结果对象 dx,第三个参数是写入的文件
+#   1.首先判断 obj的类型 字符串 /方法 
+#   2.寻找调用条件的基本规则:
+#       a.  obj 如果是字符串类型，那么有两种可能:显式字符串 和 通过getString间接调用的arsc字符串
+#           前者可以使用  get_xref_from搜寻到所有调用的 method，然后执行下面的操作
+#       b. obj 如果是 method 类型, 则该 method必然是使用了负面字符串作为参数通过下面三种方式寻找判断条件               
+#           i.调用了返回类型为 bool的函数  (查找调用者)        
+#           ii.判断是否存在条件指令  (在method内部寻找 )
+#           iii.尝试向上递归2~3层,至少找到一个带有逻辑判断的
+
+def get_logical_method(obj,dx,file):
+
+    # 对于method 类型的参数有几个情况：
+    #  1.单纯的弹窗函数，在该函数内没有什么有效判断逻辑,需要向上查找调用者函数  （向上递归）
+    #  2.作为一个调用者函数,包含了判断逻辑和调用弹窗函数的函数，可能存在免疫逻辑判断函数  （查找 bool类型的被调用者函数)
+    #  3.作为一个调用者函数，调用了弹窗函数, 自身存在逻辑判断条件，而非调用免疫逻辑判断函数 (查找有关的条件指令)
+    if type(obj)==analysis.MethodClassAnalysis or type(obj)==analysis.MethodAnalysis:
+        find_logic=0                        # 用来表示是否找到了 相应的逻辑函数
+        if obj.is_external():               # 不考虑外部函数
+            return
+
+
+        # 向下查找 bool类型的被调用函数
+        for call,meth,_ in obj.get_xref_to():          
+            if type(meth)==analysis.ExternalMethod:    #不考虑外部函数
+                continue
+            info=meth.get_information()
+            if 'return' in info and info['return']=='boolean':              # 返回类型为bool 类型
+                find_logic=find_logic+1
+                temp=obj.get_method()
+                file.write(temp.get_class_name()+temp.get_name()+" ----calling---- "+meth.get_class_name()+meth.get_name()+"\n")
+
+
+        # 查找函数内部条件跳转指令
+        encode_meth=obj.get_method()
+        file.write(encode_meth.get_class_name()+encode_meth.get_name()+"  conditional mnemonic: \n")
+        for ins in encode_meth.get_instructions():
+            if ins.get_name() in Conditionlist:                  # 寻找条件语句
+                find_logic=find_logic+1
+                file.write(ins.get_name()+ ins.get_output()+"\n")
+
+
+        # 向上查找 1次
+        if find_logic==0:
+            for class_,call,_ in obj.get_xref_from(): 
+                meth=class_.get_method_analysis(call)
+                if type(meth)==analysis.MethodClassAnalysis or type(meth)==analysis.MethodAnalysis:
+                    get_logical_method(meth,dx,file)  
+
+        return
+
+    if type(obj)==str:                              
+        get_logical_method(dx.strings[obj],dx,file)
+
+
+    elif type(obj)==analysis.StringAnalysis:               #字符串
+        for class_,call in obj.get_xref_from():            # 获取所有调用该字符串的函数，分析该函数
+            meth=class_.get_method_analysis(call)
+            if type(meth)==analysis.MethodClassAnalysis or type(meth)==analysis.MethodAnalysis:
+                get_logical_method(meth,dx,file)  
+
+    # 不是上面的类型,则出错                  
+    else:
+        print("Error argument,str or method argument is required!")
+    
+
+
+
+
+
 if __name__ == '__main__':
     # 处理apk
-    file_path = "F:\\2021summerImmunedroid\\2021winter\\com.teladoc.members_748_apps.evozi.com.apk"
-    store_file_path = "F:\\2021summerImmunedroid\\2021winter\\"
-    exception_list_path = "F:\\2021summerImmunedroid\\2021winter\\ThirdLibs.txt"
+    # file_path = "F:\\2021summerImmunedroid\\2021winter\\com.teladoc.members_748_apps.evozi.com.apk"
+    file_path = "C:\\Users\\86157\\Desktop\\example\\b.apk"
+    store_file_path = "C:\\Users\\86157\\Desktop\\example\\"
+    # store_file_path = "F:\\2021summerImmunedroid\\2021winter\\"
+    exception_list_path = "C:\\Users\\86157\\Desktop\\example\\ThirdLibs.txt"
     # file_path,store_file_path = getarg(sys.argv[1:])
     a, d, dx = get_androguard_obj(file_path)
     # test:begin
@@ -313,20 +403,40 @@ if __name__ == '__main__':
     print("get strings finish")
     # 获取资源文件中的string
     s,string = output_arsc_strings(a)
+
     print ("string length:" + str(len(string)))
     print ("s length:" + str(len(s)))
     print("get arsc strings finish")
     # 使用Textblob处理英文字符。使用Snownlp处理中文语句情感
-    output_calling_method_neg(strs)
+
+    neg_string=output_calling_method_neg(strs)
+    fout_neg_logic=open(store_file_path+"neg_string_logic.txt", "w+", encoding='utf-8')
+
+
+    # 寻找直接调用的字符串对应的判断逻辑
+    for i in range(len(neg_string)):
+        fout_neg_logic.write("search string content: " + neg_string[i]+"\n")
+        fout_neg_logic.write("search logic result: \n")
+        get_logical_method(neg_string[i],dx,fout_neg_logic)
+        fout_neg_logic.write("------separationline----------\n\n")
+
+
     print("output_negstr in program finish")
 
     fout_arsc = open(store_file_path+"arsc_result.txt", "w+", encoding='utf-8')
+
+    # 逻辑判断输出文件
+    fout_logic=open(store_file_path+"arsc_logic.txt", "w+", encoding='utf-8')
+
     print("begin to search arsc")
+
     for i in range(0,len(s)):
         # print(s[i])
         fout_arsc.write("search ID: "+s[i]+"\n")
         fout_arsc.write("search string content: " + string[i]+"\n")
         fout_arsc.write("search result: \n")
-        search_str(dx,str(s[i]),fout_arsc)
+        search_str(dx,str(s[i]),fout_arsc,fout_logic,string[i])
         fout_arsc.write("------separationline----------\n")
+
+   
     print("语义分析完成")
